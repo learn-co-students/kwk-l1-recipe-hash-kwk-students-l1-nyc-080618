bd3af2206fc43b630bf7a00b14960873b19719c6
adielletuchman <adielle.tuchman@mjbha.org>
2018-08-10 18:41:17 +0000

    Done.

---
+++ sample.rb
@@ -0,0 +1,8 @@
+perfect_10_recipe = {:almond_flour => "2 cups", :gluten_freewhole_oats => "1 cup", :kosher_salt => "1/2 tea spoon", :baking_powder => "1/2 tea spoon", :baking_soda => "1/4 tea spoon", :xanthan_gum => "1/2 tea spoon", :silvered_almonds_lightly_toasted => "1/4 cup", :mini_dark_chocolate_chips => "3/4 cup", :olive_oil => "1/2 cup", :agave => "1/4 cup"}
+
+def number_ingredients(perfect_10_recipe)
+puts perfect_10_recipe.length 
+ 
+end
+
+number_ingredients(perfect_10_recipe)
